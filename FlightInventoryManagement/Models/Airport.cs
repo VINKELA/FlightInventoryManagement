6003136f0954818ff46292519b91cd12a95ad069
Orji-kalu Kelvin <orjikalukelvin@gmail.com>
2024-11-07 01:35:07 -0500

    project set up

---
+++ FlightInventoryManagement/Models/Airport.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightInventoryManagement.Models
+{
+    public class Airport
+    {
+        public required string Name { get; set; }
+        public required string Code { get; set; }
+      
+    }
+}
