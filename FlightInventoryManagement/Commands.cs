5ae8eedaa545d4f6f136a4ab1cfb6fd482b15952
Orji-kalu Kelvin <orjikalukelvin@gmail.com>
2024-11-12 15:22:21 -0500

    program set up

---
+++ FlightInventoryManagement/Commands.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightInventoryManagement
+{
+    public static class Commands
+    {
+        public const string ListFlights = "list flights";
+        public const string GenerateOrderItenary = "generate itenary";
+        public const string EndCommand = "x";
+
+    }
+}
---
+++ FlightInventoryManagement/Program.cs
@@ -0,0 +1,59 @@
+﻿using FlightInventoryManagement.Models;
+using FlightInventoryManagement.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FlightInventoryManagement
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var endApp = false;
+            var serviceCollection = SetUP();
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+            var schedulingService = serviceProvider.GetRequiredService<IFlightScheduleService>();
+            //This loads the Flight Schedule into a graph data structure for easy transerval 
+
+            schedulingService.LoadDailySchedule();
+
+            do
+            {
+                Console.WriteLine("\nEnter one of the following commands\n\n" +
+                    $"{Commands.ListFlights} (To List Scheduled Flights)\n" +
+                    $"{Commands.GenerateOrderItenary} (To List Generated Order Itenary)\n" +
+                    $"{Commands.EndCommand}  (To End Program)\n\n");
+
+                var input = Console.ReadLine();
+                var trimmed = string.IsNullOrWhiteSpace(input) ? "" : input.Trim().ToLower();
+                switch (trimmed)
+                {
+                    case Commands.ListFlights:
+                        schedulingService.PrintSchedule();
+                        break;
+                    case Commands.GenerateOrderItenary:
+                        schedulingService.GenerateItenary();
+                        break;
+                    case Commands.EndCommand:
+                        endApp = true;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Command");
+                        break;
+                }
+            } while (!endApp);
+        }
+        static ServiceCollection SetUP()
+        {
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddSingleton<IAirportService, AirportService>();
+            serviceCollection.AddSingleton<IFlightScheduleService, FlightScheduleService>();
+            serviceCollection.AddSingleton<IOrderService, OrderService>();
+            serviceCollection.AddTransient<IAirPlaneService, AirPlaneService>();
+            serviceCollection.AddTransient<IDayService, DayService>();
+
+
+            return serviceCollection;
+        }
+    }
+}
